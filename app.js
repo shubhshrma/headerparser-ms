ebbe3746448aca70860bd2a43e1689eab6bcb447
shubhshrma <shubham.31996@gmail.com>
2017-08-25 00:00:28 +0530

    update app.js

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 //require the packages we need
 var express=require('express');
 var app=express();
+var useragent=require('useragent');
+
 
 //set the port
 var port = process.env.PORT || 3000;
 
 app.get('/',function(req,res,next){
+     
      var myIp = req.headers['X-Forwarded-For'] || 
      req.connection.remoteAddress || 
      req.socket.remoteAddress ||
@@ -14,8 +17,10 @@ app.get('/',function(req,res,next){
 
      var lang = req.headers["accept-language"].split(',')[0];
      
+     var agent = useragent.parse(req.headers['user-agent']);
+     var os = agent.toString();
 
-     res.json({"ip":myIp,"lang":lang});	
+     res.json({"ip":myIp,"lang":lang,"OS":os});	
 });
 
 app.listen(port,function(){
