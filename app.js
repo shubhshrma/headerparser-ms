ede7fd8e5cb0467773b0aad0f7dbadd0d126dcb2
shubhshrma <shubham.31996@gmail.com>
2017-08-24 23:41:18 +0530

    update app.js

--- app.js
+++ app.js
@@ -7,9 +7,17 @@ var app=express();
 var port = process.env.PORT || 3000;
 
 app.get('/',function(req,res,next){
-      res.json({'ip':req.ip});
+     var myIp = req.headers['X-Forwarded-For'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+
+     var lang = req.headers["accept-language"].split(',')[0];
+     
+
+     res.json({"ip":myIp,"lang":lang});	
 });
 
 app.listen(port,function(){
-	console.log("App listening on port" + port);
+	console.log("App listening on port: " + port);
 });
